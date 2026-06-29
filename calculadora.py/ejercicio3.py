cef5f8866e718396f5749d0afdf78eded4200ae6
Miguel Angel Paulino <miguelpaulino1122@gmail.com>
2026-06-28 23:04:16 -0400

    listo

---
+++ calculadora.py/ejercicio3.py
@@ -0,0 +1,20 @@
+# Programa: Intercambio de valores
+
+# Declaración de variables
+a = 5
+b = 10
+
+# Mostrar los valores originales
+print("===== ANTES DEL INTERCAMBIO =====")
+print("Valor de a:", a)
+print("Valor de b:", b)
+
+# Intercambiar los valores utilizando una variable auxiliar
+auxiliar = a
+a = b
+b = auxiliar
+
+# Mostrar los nuevos valores
+print("\n===== DESPUÉS DEL INTERCAMBIO =====")
+print("Valor de a:", a)
+print("Valor de b:", b)
