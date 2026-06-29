81461c97a2e134f3558994edd74e30ead783bbe8
Miguel Angel Paulino <miguelpaulino1122@gmail.com>
2026-06-28 22:56:47 -0400

    listo

---
+++ calculadora.py/ejercicio2.py
@@ -0,0 +1,12 @@
+# Conversor de Edad a Días
+
+# Entrada de datos
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad en años: "))
+
+# Cálculo de los días vividos
+dias_vividos = edad * 365
+
+# Salida de resultados
+print("\n===== RESULTADO =====")
+print(f"Hola {nombre}, has vivido aproximadamente {dias_vividos} días.")
